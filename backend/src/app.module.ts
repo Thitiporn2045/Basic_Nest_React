3ae408bb32b1de4295e9e6067f8ad5b90675d996
Thitiporn2045 <142084816+Thitiporn2045@users.noreply.github.com>
2025-07-06 11:16:03 +0700

    Add TypeORM configuration and User entity for database integration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,26 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/user.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432',10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_DATABASE || 'nestdb',
+      entities: [User],
+      synchronize: true, 
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })

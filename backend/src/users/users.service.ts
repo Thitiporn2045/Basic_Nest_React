90db88d7d4206fd8cfa5761e322a415d9fbb2a63
Thitiporn2045 <142084816+Thitiporn2045@users.noreply.github.com>
2025-07-06 11:40:36 +0700

    Implement UsersController and UsersService with findAll method for user retrieval

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(User)
+        private usersRepository: Repository<User>,
+    ) {}
+
+    async findAll(): Promise<User[]> {
+        const count = await this.usersRepository.count();
+        if (count === 0) {
+            console.log('No users found. Creating default users...');
+            await this.usersRepository.save([
+                {name: 'THITIPORN THIAMJAN'}
+            ]);
+        }
+        return this.usersRepository.find();
+    }
+}
